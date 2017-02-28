ec373ec84e3378d46552741582241ebe2f940fb3
Jacek Kuzemczak <jacek@dataplan.co.uk>
2017-02-28 09:20:48 +0000

    Added run length decode function

--- src/filter/run_length_decode.php
+++ src/filter/run_length_decode.php
@@ -19,7 +19,33 @@ class run_length_decode extends filter {
 	 * @inheritDoc
 	 */
 	public function decode(string $data): string {
-		throw new \Exception('Not yet implemented');
+		// Based (heavily) on https://github.com/zendframework/zf1/blob/master/library/Zend/Pdf/Filter/RunLength.php
+
+		$data_length = strlen($data);
+		$result = '';
+		$offset = 0;
+
+		while ($offset < $data_length) {
+			$length = ord($data[$offset]);
+
+			++$offset;
+
+			if ($length === 128) {
+				break;
+			} else if ($length < 128) {
+				++$length;
+
+				$result .= substr($data, $offset, $length);
+
+				$offset += $length;
+			} else if ($length > 128) {
+				$result .= str_repeat($data[$offset], (257 - $length));
+
+				++$offset;
+			}
+		}
+
+		return $result;
 	}
 
 }
---
+++ tests/filter/RunLengthDecodeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use dataplan\pdfStreamDecode\filter\run_length_decode;
+
+class RunLengthDecodeTest extends StreamDecodeTestCase {
+
+	public function testDecode() {
+		$filter = new run_length_decode([]);
+
+		$expected_result = 'AAAABBBBBB123123123';
+		$input = chr(257 - 4) . 'A' . chr(257 - 6) . 'B' . "\x08" . '123123123' . "\x80";
+
+		$actual_result = $filter->decode($input);
+
+		$this->assertInternalType('string', $actual_result);
+		$this->assertSame($expected_result, $actual_result);
+	}
+
+}
